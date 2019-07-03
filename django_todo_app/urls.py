b05c09d37766001e1fb528154646d809d9042c63
Ubuntu <ubuntu@ip-172-31-45-126.ec2.internal>
2019-07-03 12:43:35 +0000

    Added the 'todo' app

--- django_todo_app/urls.py
+++ django_todo_app/urls.py
@@ -1,5 +1,4 @@
-"""django_todo_app URL Configuration
-
+"""django_todo URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
@@ -15,7 +14,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from todo.views import say_hello
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'message$', say_hello)
 ]
